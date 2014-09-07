61219ecd294dc3dcaed090b2621bed4f5bbc166f
thorntonv <thorntonv@mechaverse.org>
2014-09-06 23:36:46 -0700

    Removed log4j dependencies.

--- simulation/mechaverse-simulation-common/src/test/java/org/mechaverse/simulation/common/opencl/DeviceUtilTest.java
+++ simulation/mechaverse-simulation-common/src/test/java/org/mechaverse/simulation/common/opencl/DeviceUtilTest.java
@@ -2,17 +2,16 @@ package org.mechaverse.simulation.common.opencl;
 
 import static org.junit.Assert.assertTrue;
 
-import org.apache.log4j.Logger;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for {@link DeviceUtil}.
- *
- * @author thorntonv@mechaverse.org
  */
 public class DeviceUtilTest {
 
-  private Logger logger = Logger.getLogger(DeviceUtilTest.class);
+  private Logger logger =  LoggerFactory.getLogger(DeviceUtilTest.class);
 
   @Test
   public void getDeviceInfo() {
