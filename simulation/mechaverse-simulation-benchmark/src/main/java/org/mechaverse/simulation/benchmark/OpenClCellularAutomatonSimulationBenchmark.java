e127f46cd6709a2f149745e2b0bccd3470fe8500
Vance <thorntonv@mechaverse.org>
2018-07-29 01:00:10 -0700

    Replaced caliper benchmark framework with JMH.

--- simulation/mechaverse-simulation-benchmark/src/main/java/org/mechaverse/simulation/benchmark/OpenClCellularAutomatonSimulationBenchmark.java
+++ simulation/mechaverse-simulation-benchmark/src/main/java/org/mechaverse/simulation/benchmark/OpenClCellularAutomatonSimulationBenchmark.java
@@ -1,88 +1,106 @@
 package org.mechaverse.simulation.benchmark;
 
-import com.google.caliper.Benchmark;
-import com.google.caliper.Param;
-import com.google.caliper.runner.CaliperMain;
 import com.jogamp.opencl.CLPlatform;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.math3.random.RandomGenerator;
 import org.apache.commons.math3.random.Well19937c;
 import org.mechaverse.cellautomaton.model.CellularAutomatonDescriptor;
 import org.mechaverse.simulation.common.cellautomaton.simulation.CellularAutomatonDescriptorReader;
 import org.mechaverse.simulation.common.cellautomaton.simulation.CellularAutomatonSimulationUtil;
 import org.mechaverse.simulation.common.cellautomaton.simulation.opencl.OpenClCellularAutomatonSimulator;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.runner.RunnerException;
 
-public class OpenClCellularAutomatonSimulationBenchmark extends Benchmark {
+@Fork(value = 1, warmups = 0)
+@Threads(1)
+@Warmup(iterations = 3)
+public class OpenClCellularAutomatonSimulationBenchmark {
 
   private static final String DESCRIPTOR_XML_FILENAME = "boolean4.xml";
-  
-  @Param(value = {"175"}) int iterationsPerUpdate;
-  @Param(value = {"1024"}) int numAutomata;
-  @Param(value = {"16"}) int size;
-  @Param(value = {"8"}) int width;
-  @Param(value = {"8"}) int height;
 
-  private int[] input;
-  private int[] output;
-  private OpenClCellularAutomatonSimulator simulator;
+  @State(Scope.Benchmark)
+  public static class ExecutionPlan {
 
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
+    @Param(value = {"175"}) int iterationsPerUpdate;
+    @Param(value = {"1024"}) int numAutomata;
+    @Param(value = {"16"}) int size;
+    @Param(value = {"8"}) int width;
+    @Param(value = {"8"}) int height;
 
-    final RandomGenerator random = new Well19937c();
+    private int[] input;
+    private int[] output;
+    private OpenClCellularAutomatonSimulator simulator;
 
-    CellularAutomatonDescriptor descriptor = CellularAutomatonDescriptorReader.read(
-        ClassLoader.getSystemResourceAsStream(DESCRIPTOR_XML_FILENAME));
-    descriptor.setIterationsPerUpdate(iterationsPerUpdate);
-    descriptor.setWidth(width);
-    descriptor.setHeight(height);
+    @Setup(Level.Trial)
+    public void setUp() throws IOException {
+      final RandomGenerator random = new Well19937c();
 
-    try {
-      simulator = new OpenClCellularAutomatonSimulator(numAutomata, size, size, 
-          CLPlatform.getDefault().getMaxFlopsDevice(), descriptor);
+      CellularAutomatonDescriptor descriptor = CellularAutomatonDescriptorReader.read(
+          ClassLoader.getSystemResourceAsStream(DESCRIPTOR_XML_FILENAME));
+      descriptor.setIterationsPerUpdate(iterationsPerUpdate);
+      descriptor.setWidth(width);
+      descriptor.setHeight(height);
 
-      input = CellularAutomatonSimulationUtil.randomState(size, random);
-      output = CellularAutomatonSimulationUtil.randomState(size, random);
-      int[] ioMap = new int[size];
-      for (int idx = 0; idx < size; idx++) {
-        ioMap[idx] = random.nextInt(simulator.getAutomatonStateSize());
+      try {
+        simulator = new OpenClCellularAutomatonSimulator(numAutomata, size, size,
+            CLPlatform.getDefault().getMaxFlopsDevice(), descriptor);
+
+        input = CellularAutomatonSimulationUtil.randomState(size, random);
+        output = CellularAutomatonSimulationUtil.randomState(size, random);
+        int[] ioMap = new int[size];
+        for (int idx = 0; idx < size; idx++) {
+          ioMap[idx] = random.nextInt(simulator.getAutomatonStateSize());
+        }
+
+        for (int idx = 0; idx < numAutomata; idx++) {
+          int[] state = CellularAutomatonSimulationUtil.randomState(
+              simulator.getAutomatonStateSize(), random);
+          simulator.setAutomatonState(idx, state);
+          simulator.setAutomatonInputMap(idx, ioMap);
+          simulator.setAutomatonOutputMap(idx, ioMap);
+        }
+      } catch (Throwable t) {
+        t.printStackTrace();
       }
+    }
 
-      for (int idx = 0; idx < numAutomata; idx++) {
-        int[] state = CellularAutomatonSimulationUtil.randomState(
-            simulator.getAutomatonStateSize(), random);
-        simulator.setAutomatonState(idx, state);
-        simulator.setAutomatonInputMap(idx, ioMap);
-        simulator.setAutomatonOutputMap(idx, ioMap);
-      }
-    } catch (Throwable t) {
-      t.printStackTrace();
+    @TearDown(Level.Trial)
+    public void tearDown() {
+      simulator.close();
     }
   }
 
-  @Override
-  protected void tearDown() throws Exception {
-    super.tearDown();
-    simulator.close();
-  }
 
-  @SuppressWarnings("unused")
-  public int timeUpdate(int reps) {
+  @Benchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public int run(ExecutionPlan plan) {
     int dummy = 0;
-    for (int i = 0; i < reps; i++) {
-      for (int idx = 0; idx < numAutomata; idx++) {
-        simulator.setAutomatonInput(idx, input);
-      }
-      simulator.update();
-      for (int idx = 0; idx < numAutomata; idx++) {
-        simulator.getAutomatonOutput(idx, output);
-        dummy += output[0];
-      }
+    for (int idx = 0; idx < plan.numAutomata; idx++) {
+      plan.simulator.setAutomatonInput(idx, plan.input);
+    }
+    plan.simulator.update();
+    for (int idx = 0; idx < plan.numAutomata; idx++) {
+      plan.simulator.getAutomatonOutput(idx, plan.output);
+      dummy += plan.output[0];
     }
     return dummy;
   }
 
-  public static void main(String[] args) {
-    CaliperMain.main(OpenClCellularAutomatonSimulationBenchmark.class, args);
+  public static void main(String[] args) throws IOException, RunnerException {
+    org.openjdk.jmh.Main.main(args);
   }
 }
